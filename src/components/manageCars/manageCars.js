0252c0b381c01247a1118a7e0f381b4f3d3f7d7e
Amit Markovich <amitmarkovich92@gmail.com>
2019-03-16 17:57:21 +0200

    Finished managrCars

--- src/components/manageCars/manageCars.js
+++ src/components/manageCars/manageCars.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { removeCar,addCar,carsLoading,getAllCars } from "../../actions/carsActions";
+import {
+  removeCar,
+  addCar,
+  carsLoading,
+  getAllCars
+} from "../../actions/carsActions";
 import Spinner from "../common/spinner/spinner";
 import ReactTable from "react-table";
 
@@ -9,47 +14,57 @@ class ManageCars extends Component {
     super(props);
 
     this.state = {
-        username:'',
-        carNumber: "",
-        errorAdd: "",
-        successAdd: "",
-        cars: [],
-        usersColums: [
-          {
-            Header: "Car number",
-            accessor: "carNumber" // String-based value accessors!
-          },
-          {
-            Header: "Is inside",
-            accessor: "isInside"
-          },
-          {
-            id: 'delete',
-            accessor: "carNumber",
-            Cell: ({value}) => (<div onClick={()=>this.onSubmitRemove({value})}><img class="garbage"  src="/pictures/garbage.png" title="Remove car" alt="garbage"></img></div>)
-          }
-        ]
-      }
+      username: "",
+      carNumber: "",
+      errorAdd: "",
+      successAdd: "",
+      cars: [],
+      usersColums: [
+        {
+          Header: "Car number",
+          accessor: "carNumber" // String-based value accessors!
+        },
+        {
+          Header: "Is inside",
+          accessor: "isInside"
+        },
+        {
+          id: "delete",
+          accessor: "carNumber",
+          Cell: ({ value }) => (
+            <div onClick={() => this.onSubmitRemove({ value })}>
+              <img
+                class="garbage"
+                src="/pictures/garbage.png"
+                title="Remove car"
+                alt="garbage"
+              />
+            </div>
+          )
+        }
+      ]
     };
-  
+  }
 
   componentDidMount() {
+    if (!sessionStorage.getItem("userData")) {
+      this.props.history.push("/");
+    }
 
-        if (!sessionStorage.getItem("userData")) {
-          this.props.history.push("/");
-        }
-    
-        this.setState({
-          username: this.props.userData.userData.username,
-          firstName: this.props.userData.userData.firstName,
-          lastName: this.props.userData.userData.lastName,
-          capcity:this.props.userData.userData.capcity
-        },()=>{
-          this.props.carsLoading();
-          this.props.getAllCars(this.state.username);
-        });
+    this.setState(
+      {
+        username: this.props.userData.userData.username,
+        firstName: this.props.userData.userData.firstName,
+        lastName: this.props.userData.userData.lastName,
+        capcity: this.props.userData.userData.capcity
+      },
+      () => {
+        this.props.carsLoading();
+        this.props.getAllCars(this.state.username);
+      }
+    );
   }
-  
+
   componentWillReceiveProps(nextProps) {
     if (!sessionStorage.getItem("userData")) {
       this.props.history.push("/");
@@ -57,9 +72,9 @@ class ManageCars extends Component {
 
     this.setState({
       cars: nextProps.carsData.cars,
-      errorAdd:nextProps.carsData.errorAdd,
-      successAdd:nextProps.carsData.successAdd
-    })
+      errorAdd: nextProps.carsData.errorAdd,
+      successAdd: nextProps.carsData.successAdd
+    });
   }
 
   onChange = e => {
@@ -75,9 +90,9 @@ class ManageCars extends Component {
     this.props.carsLoading();
     this.props.addCar(data);
   };
-  
-  onSubmitRemove =({value}) => {
-    console.log(value)
+
+  onSubmitRemove = ({ value }) => {
+    console.log(value);
     const data = {
       username: this.state.username,
       carNumber: value
@@ -87,61 +102,54 @@ class ManageCars extends Component {
     this.props.removeCar(data);
   };
 
-
   render() {
-      return (
-        <div className="dashboard">
-    <h1>Manage Cars</h1>
-    <br/>
-          <h1>Add new car</h1>
-          <form onSubmit={this.onSubmitAdd}>
-            <input
-              required={true}
-              type="text"
-              name="carNumber"
-              placeholder="Enter car number"
-              value={this.state.carNumber}
-              onChange={this.onChange}
-            />
-            <input type="submit" value="Submit" />
-            <p>
-              {this.state.errorAdd}
-              {this.state.successAdd}
-            </p>
-          </form>   
-          <h1>Remove car</h1>
-          {/* <form onSubmit={this.onSubmitRemove}>
-            <input
-              required={true}
-              type="text"
-              name="carNumber"
-              value={this.state.carNumber}
-              onChange={this.onChange}
-            />
-            <input type="submit" value="Submit" />
-            <p>
-              {this.state.error}
-              {this.state.success}
-            </p>
-          </form> */}
+    return (
+      <div className="manage">
+        <div className="manage__content">
+          <h3 className="manage__header">Manage Cars</h3>
+          <div className="manage__add">
+            <h1 className="manage__add__header">Add new car</h1>
+            <form onSubmit={this.onSubmitAdd} className="manage__add__form">
+              <input
+                required={true}
+                type="text"
+                name="carNumber"
+                placeholder="Enter car number"
+                value={this.state.carNumber}
+                onChange={this.onChange}
+                className="manage__add__form__input"
+              />
+              <input type="submit" value="Submit" className="manage__add__form__submit"/>
+              <p>
+                {this.state.errorAdd}
+                {this.state.successAdd}
+              </p>
+            </form>
+          </div>
 
-          {this.props.loading?<Spinner />:  <ReactTable data={this.state.cars} columns={this.state.usersColums} />}
-        
-   
+          {this.props.loading ? (
+            <Spinner />
+          ) : (
+            <ReactTable
+              data={this.state.cars}
+              columns={this.state.usersColums}
+              className='manage__table'
+            />
+          )}
         </div>
-      );
-    
+      </div>
+    );
   }
 }
 const mapStateToProps = state => {
   return {
     carsData: state.carsData,
     loading: state.carsData.loading,
-    userData: state.userData,
+    userData: state.userData
   };
 };
 
 export default connect(
   mapStateToProps,
-  { getAllCars,removeCar,carsLoading,addCar }
+  { getAllCars, removeCar, carsLoading, addCar }
 )(ManageCars);
