3b1b02e9fd7ce243dfaab08dc14f19a1e1744d70
Amit Markovich <amitmarkovich92@gmail.com>
2019-03-16 17:12:42 +0200

    Fnished edit user

--- src/components/editUser/editUser.js
+++ src/components/editUser/editUser.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import {editUserLoading,editUser } from "../../actions/userActions";
+import { editUserLoading, editUser } from "../../actions/userActions";
 import Spinner from "../common/spinner/spinner";
 
 class EditUser extends Component {
@@ -8,41 +8,39 @@ class EditUser extends Component {
     super(props);
 
     this.state = {
-        username:'',
-        firstName:'',
-        lastName:'',
-        password:'',
-        capacity:'',
-        error: "",
-        success: ""
-      }
+      username: "",
+      firstName: "",
+      lastName: "",
+      password: "",
+      capacity: "",
+      error: "",
+      success: ""
     };
-  
+  }
 
   componentDidMount() {
-
     if (!sessionStorage.getItem("userData")) {
       this.props.history.push("/");
     }
 
     this.setState({
-      username:this.props.userData.userData.username,
-      firstName:this.props.userData.userData.firstName,
-      lastName:this.props.userData.userData.lastName,
-      password:this.props.userData.userData.password,
-      capacity:this.props.userData.userData.capacity
+      username: this.props.userData.userData.username,
+      firstName: this.props.userData.userData.firstName,
+      lastName: this.props.userData.userData.lastName,
+      password: this.props.userData.userData.password,
+      capacity: this.props.userData.userData.capacity
     });
   }
-  
+
   componentWillReceiveProps(nextProps) {
     if (!sessionStorage.getItem("userData")) {
       this.props.history.push("/");
     }
 
     this.setState({
-      error:nextProps.userData.errorEdit,
-      success:nextProps.userData.successEdit
-    })
+      error: nextProps.userData.errorEdit,
+      success: nextProps.userData.successEdit
+    });
   }
 
   onChange = e => {
@@ -52,80 +50,101 @@ class EditUser extends Component {
   onSubmit = e => {
     e.preventDefault();
     const data = {
-      username:this.state.username,
-      firstName:this.state.firstName,
-      lastName:this.state.lastName,
-      password:this.state.password,
-      capacity:this.state.capacity
+      username: this.state.username,
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      password: this.state.password,
+      capacity: this.state.capacity
     };
     this.props.editUserLoading();
     this.props.editUser(data);
   };
-
 
   render() {
     if (this.props.loading) {
       return <Spinner />;
     } else {
       return (
-        <div className="dashboard">
+        <div className="edituser">
+        <div className="edituser__content">
+        <div className="edituser__header">
+            <h1>Edit user</h1>
+          </div>
+          <form onSubmit={this.onSubmit} className="edituser__form">
+            <div className="edituser__form__group">
+              <label className="edituser__form__label">username:</label>
+              <input
+                required={true}
+                type="text"
+                disabled={true}
+                name="username"
+                value={this.state.username}
+                onChange={this.onChange}
+                className="edituser__form__input"
+              />
+            </div>
+            <div className="edituser__form__group">
+              <label className="edituser__form__label" htmlFor='password'>password:</label>
+              <input
+                required={true}
+                type="text"
+                name="password"
+                value={this.state.password}
+                onChange={this.onChange}
+                className="edituser__form__input"
+                id='password'
+              />
+            </div>
+            <div className="edituser__form__group">
+              <label className="edituser__form__label" htmlFor="firstName">
+                First name:
+              </label>
+              <input
+                required={true}
+                type="text"
+                name="firstName"
+                value={this.state.firstName}
+                onChange={this.onChange}
+                className="edituser__form__input"
+                id="firstName"
+              />
+            </div>
 
-          <h1>Edit user</h1>
-          <form onSubmit={this.onSubmit}>
-            <label>username: </label>
-             <input
-              required={true}
-              type="text"
-              disabled={true}
-              name="username"
-              value={this.state.username}
-              onChange={this.onChange}
-            />
-             <br/>
-             <label>password: </label>
-             <input
-              required={true}
-              type="text"
-              name="password"
-              value={this.state.password}
-              onChange={this.onChange}
-            />
-            <br/>
-            <label>First name: </label>
-            <input
-              required={true}
-              type="text"
-              name="firstName"
-              value={this.state.firstName}
-              onChange={this.onChange}
-            />
-            <br/>
-             <label>Last name: </label>
-            <input
-              required={true}
-              type="text"
-              name="lastName"
-              value={this.state.lastName}
-              onChange={this.onChange}
-            />
-            <br/>
-            <label>capcity: </label>
-             <input
-              required={true}
-              type="text"
-              name="capacity"
-              value={this.state.capacity}
-              onChange={this.onChange}
-            />
-            <br/>
-            <input type="submit" value="Submit" />
+            <div className="edituser__form__group">
+              <label className="edituser__form__label" htmlFor='lastName'>Last name:</label>
+              <input
+                required={true}
+                type="text"
+                name="lastName"
+                value={this.state.lastName}
+                onChange={this.onChange}
+                className="edituser__form__input"
+                id='lastName'
+              />
+            </div>
+
+            <div className="edituser__form__group">
+              <label className="edituser__form__label" htmlFor='capcity'>capcity:</label>
+              <input
+                required={true}
+                type="text"
+                name="capacity"
+                value={this.state.capacity}
+                onChange={this.onChange}
+                className="edituser__form__input"
+                id='capcity'
+              />
+            </div>
+
+            <div className="edituser__form__submit">
+              <input type="submit" value="Submit" className="edituser__form__submit-input"/>
+            </div>
             <p>
               {this.state.error}
               {this.state.success}
             </p>
           </form>
-
-        
+        </div>
         </div>
       );
     }
@@ -135,10 +154,11 @@ const mapStateToProps = state => {
   return {
     carsData: state.carsData,
     loading: state.userData.loading,
-    userData: state.userData,
+    userData: state.userData
   };
 };
 
 export default connect(
-  mapStateToProps,{editUserLoading,editUser}
+  mapStateToProps,
+  { editUserLoading, editUser }
 )(EditUser);
